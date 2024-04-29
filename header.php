6cda9519ed0964f4c45498fc233e646712146a7a
Ben <benfyfe1999@gmail.com>
2024-04-29 14:51:01 +0100

    Initial site commit

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
+
+    <!-- Title Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
+
+    <!-- Page Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+    <title>T2</title>
+</head>
+<body>
+    <div class="header-container">
+        <img class="header-img" src="<?php echo esc_url( get_template_directory_uri() . '/img/T2-logo-simple.png' ); ?>" alt="" >
+        <h1 class="cinzel">Team Tomahawk</h1>
+        <div class="nav-links">
+            <a class="nav-item" href="">Home</a>
+            <a class="nav-item" href="">Story</a>
+            <a class="nav-item" href="">Blog</a>
+            <a class="nav-item" href="">Reccomendations</a>
+            <a class="nav-join nav-item" href="">Want to join?</a>
+        </div>
+    </div>
+</body>
+</html>
